0c883ac74b5d552af60e92e7765a34c534f27519
Otchy <id+github@otchy.net>
2019-03-01 00:29:40 +0900

    0.0.3: Support webpack.config.js

--- bin/init.js
+++ bin/init.js
@@ -2,15 +2,42 @@
 
 const execSync = require('child_process').execSync;
 const exec = command => execSync(command).toString();
+const fs = require('fs');
 
 // install modules
-console.log(exec('npm install --save-dev json'))
-const json = './node_modules/.bin/json';
+const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+const requiredModules = [
+    'json', 'ncp', 'rmdir',
+    '@babel/core', '@babel/preset-env', 'babel-loader',
+    'webpack', 'webpack-cli'
+];
+const installModules = requiredModules.filter(
+    m => !(packageJson.devDependencies && packageJson.devDependencies[m]));
+if (installModules.length > 0) {
+    console.log(`Installing following modules: ${installModules.join(',')}`);
+    console.log(exec(`npm install --save-dev ${installModules.join(' ')}`));
+}
 
-console.log(exec('npm install --save-dev @babel/core @babel/preset-env babel-loader'))
-console.log(exec('npm install --save-dev ncp rmdir'))
-console.log(exec('npm install --save-dev webpack webpack-cli'))
+// add scripts to package.json
+const addScripts = ['build', 'dist'].filter(
+    s => !(packageJson.scripts && packageJson.scripts[s])
+);
+if (addScripts.length > 0) {
+    const json = './node_modules/.bin/json';
+    console.log(`Adding following scripts: ${addScripts.join(',')}`);
+    addScripts.forEach(s => 
+        console.log(exec(`${json} -I -f package.json -e 'this.scripts.${s}="${s}-chrome-extension"'`))
+    );
+}
 
-// add scripts
-console.log(exec(`${json} -I -f package.json -e 'this.scripts.build="build-chrome-extension"'`));
-console.log(exec(`${json} -I -f package.json -e 'this.scripts.dist="dist-chrome-extension"'`));
+// create webpack.config.js
+const webpackConfigJsFile = 'webpack.config.js';
+if (!fs.existsSync(`./${webpackConfigJsFile}`)) {
+    console.log(`Creating ${webpackConfigJsFile}`);
+    const paths = __dirname.split('/');
+    paths.pop();
+    paths.push('resources');
+    const resourcesPath = `${paths.join('/')}`;
+    const webpackConfigJs = fs.readFileSync(`${resourcesPath}/${webpackConfigJsFile}`);
+    fs.writeFileSync(`./${webpackConfigJsFile}`, webpackConfigJs);
+}
---
+++ resources/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        popup: path.resolve('src/js/popup.js'),
+        background: path.resolve('src/js/background.js'),
+        options: path.resolve('src/js/options.js'),
+        page: path.resolve('src/js/page.js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: path.resolve('build/js'),
+        filename: '[name].js',
+    }
+};
